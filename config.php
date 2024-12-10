12c251ece8d235415241ada82b4288b09e5683ff
varshitha-123-tech <kurravarshita@gmail.com>
2024-12-10 11:35:19 -0600

    Updated code to use config.php for database connectivity and improved security

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 // Database connection using provided credentials
-$host = 'bqadpx2zv3iix7vczpo8-mysql.services.clever-cloud.com';
-$database = 'bqadpx2zv3iix7vczpo8';
-$username = 'uppbsxliffzcjdrk';
-$password = 'qCkygqkskoYYpCZiitga';
-$port = 3306;
+$host ='bqadpx2zv3iix7vczpo8-mysql.services.clever-cloud.com';
+$database ='bqadpx2zv3iix7vczpo8';
+$username ='uppbsxliffzcjdrk';
+$password ='qCkygqkskoYYpCZiitga';
+$port =3306;
 
 // Create connection
 $conn = new mysqli($host, $username, $password, $database, $port);
